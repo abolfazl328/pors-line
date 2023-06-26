4a70ea51fbb3583fa0722f47a8d2d52861a127c2
SajedehNorouzi <sn.norouzi20@gmail.com>
2023-06-27 00:09:48 +0330

    register changed

--- pages/register/register.js
+++ pages/register/register.js
@@ -1,37 +1,24 @@
-let submitButton = document.querySelector("#submitButton");
-submitButton.addEventListener("click", validateform);
-
 function validateform() {
-	console.log("******");
 	var password = document.register.pass.value;
 	var rePassword = document.register.repass.value;
 	var email = document.register.email.value;
-	var atposition = x.indexOf("@");
-	var dotposition = x.lastIndexOf(".");
 
 	if (password == null || rePassword == "" || email == "") {
 		alert("لطفا همه ی فیلد ها را پر کنید");
-		return false;
+		location.reload();
+		// return false;
 	} else if (password.length < 6) {
 		alert("رمز عبور باید از 6 کاراکتر بیشتر باشد");
-		location.reload();
-		return false;
-	}
-	if (password != rePassword) {
+		// return false;
+	} else if (password.length > 32) {
+		alert("رمز عبور نباید از 32 کاراکتر بیشتر باشد");
+		// return false;
+	} else if (password != rePassword) {
 		alert("رمز عبور وارد شده با تکرار ان مطابقت ندارد");
-		return false;
-	}
-	if (
-		atposition < 1 ||
-		dotposition < atposition + 2 ||
-		dotposition + 2 >= x.length
-	) {
-		alert(
-			"لطفا یک ادرس ایمیل معتبر وارد کنید \n atpostion:" +
-				atposition +
-				"\n dotposition:" +
-				dotposition
-		);
-		return false;
+		// return false;
+	} else {
+		window.location.href = "https://www.google.com";
 	}
 }
+let submitButton = document.register.submit;
+submitButton.addEventListener("click", validateform);
