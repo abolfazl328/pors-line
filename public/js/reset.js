91b8d95f40637924bc391b504067856419cb24eb
SajedehNorouzi <sn.norouzi20@gmail.com>
2023-06-28 21:40:11 +0330

    reset password form

---
+++ public/js/reset.js
@@ -0,0 +1,33 @@
+const form = document.getElementById("reset-form");
+const passwordInput = document.getElementById("password");
+const confirmPasswordInput = document.getElementById("confirm-password");
+const errorContainer = document.getElementById("error-container");
+
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	// validate password
+	if (passwordInput.value.length < 6) {
+		showError("رمز عبور باید از 6 کاراکتر بیش تر باشد\n");
+		return;
+	}
+
+	if (passwordInput.value.length > 32) {
+		showError("رمز عبور باید از 32 کاراکتر کمتر باشد\n");
+		return;
+	}
+
+	// validate confirm password
+	if (confirmPasswordInput.value !== passwordInput.value) {
+		showError("رمز عبور وارد شده با تکرار ان مطابقت ندارد\n");
+		return;
+	}
+
+	// all input fields pass validation, submit the form
+	form.submit();
+});
+
+function showError(errorMessage) {
+	errorContainer.innerHTML = errorMessage;
+	errorContainer.style.display = "block";
+}
