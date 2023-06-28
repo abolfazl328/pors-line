752bc65c42353c62cd2338f2a3603c40e251b38e
SajedehNorouzi <sn.norouzi20@gmail.com>
2023-06-28 15:59:22 +0330

    register show message instead of popup

--- pages/register/register.js
+++ pages/register/register.js
@@ -1,56 +1,41 @@
 const form = document.getElementById("registration-form");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const confirmPasswordInput = document.getElementById("confirm-password");
+const errorContainer = document.getElementById("error-container");
 
 form.addEventListener("submit", (event) => {
 	event.preventDefault();
 
-	const emailInput = document.getElementById("email");
-	const passwordInput = document.getElementById("password");
-	const confirmPasswordInput = document.getElementById("confirm-password");
-
-	let errorMessage = "";
-
+	// validate email
 	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 	if (!emailRegex.test(emailInput.value)) {
-		errorMessage = "لطفا یک ادرس ایمیل معتبر وارد کنید\n";
+		showError("لطفا یک ادرس ایمیل معتبر وارد کنید\n");
+		return;
 	}
 
+	// validate password
 	if (passwordInput.value.length < 6) {
-		errorMessage = "رمز عبور باید از 6 کاراکتر بیش تر باشد\n";
+		showError("رمز عبور باید از 6 کاراکتر بیش تر باشد\n");
+		return;
 	}
 
 	if (passwordInput.value.length > 32) {
-		errorMessage = "رمز عبور باید از 32 کاراکتر کمتر باشد\n";
+		showError("رمز عبور باید از 32 کاراکتر کمتر باشد\n");
+		return;
 	}
 
+	// validate confirm password
 	if (confirmPasswordInput.value !== passwordInput.value) {
-		errorMessage = "رمز عبور وارد شده با تکرار ان مطابقت ندارد\n";
+		showError("رمز عبور وارد شده با تکرار ان مطابقت ندارد\n");
+		return;
 	}
 
-	if (errorMessage !== "") {
-		alert(errorMessage);
-	} else {
-		form.submit();
-	}
+	// all input fields pass validation, submit the form
+	form.submit();
 });
 
-// function validateform() {
-// 	var password = document.register.pass.value;
-// 	var rePassword = document.register.repass.value;
-// 	var email = document.register.email.value;
-
-// 	if (password == null || rePassword == "" || email == "") {
-// 		alert("لطفا همه ی فیلد ها را پر کنید");
-// 		// return false;
-// 	} else if (password.length < 6) {
-// 		alert("رمز عبور باید از 6 کاراکتر بیشتر باشد");
-// 		// return false;
-// 	} else if (password.length > 32) {
-// 		alert("رمز عبور نباید از 32 کاراکتر بیشتر باشد");
-// 		// return false;
-// 	} else if (password != rePassword) {
-// 		alert("رمز عبور وارد شده با تکرار ان مطابقت ندارد");
-// 		// return false;
-// 	}
-// }
-// let submitButton = document.register.submit;
-// submitButton.addEventListener("click", validateform);
+function showError(errorMessage) {
+	errorContainer.innerHTML = errorMessage;
+	errorContainer.style.display = "block";
+}
