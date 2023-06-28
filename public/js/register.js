d558bf921b0adc87d07507f7f69ee738faeffed4
SajedehNorouzi <sn.norouzi20@gmail.com>
2023-06-28 22:25:35 +0330

    fix register problems

--- public/js/register.js
+++ public/js/register.js
@@ -38,4 +38,5 @@ form.addEventListener("submit", (event) => {
 function showError(errorMessage) {
 	errorContainer.innerHTML = errorMessage;
 	errorContainer.style.display = "block";
+	errorContainer.classList.add("error");
 }
